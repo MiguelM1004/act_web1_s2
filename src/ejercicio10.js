497f4b82655ba568763255f571b252c04dda4d03
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2025-07-24 19:29:04 -0500

    se finaliza

--- src/ejercicio10.js
+++ src/ejercicio10.js
@@ -2,7 +2,7 @@ let usuario = "admin";
 let contrasena = "1234";
 
 if (usuario === "admin" && contrasena === "1234") {
-    console.log("acceso concedido");
+    console.log("accesos concedido");
 } else {
-    console.log("acceso denegado");
+    console.log("accesos denegado");
 }
